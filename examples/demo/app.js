5639da2a8ac4f989a5d8037968b041eec5491a9f
gilbox <birmangil@gmail.com>
2015-03-29 18:19:29 -0500

    first commit

---
+++ examples/demo/app.js
@@ -0,0 +1,43 @@
+var React = require('react');
+var SparkScroll = require('react-spark-scroll');
+
+var App = React.createClass({
+
+  getInitialState() {
+    return {
+      actionType: null
+    }
+  },
+
+  render: function () {
+    var styles = {
+      height:'1000px'
+    };
+
+    if (this.state.actionType) {
+      styles.backgroundColor = this.state.actionType === 'onUp' ? '#ffc' : '#cff';
+    }
+
+    return (
+      <div style={styles}>
+        <div style={{height:'200px'}}></div>
+        <SparkScroll
+          timeline={{
+            topTop:{
+              opacity:'0.3',
+              transform:'translateX(151px)',
+              color:'#00f',
+              'onUp,onDown': (actionType) => this.setState({actionType}) },
+            centerCenter:{
+              opacity:'1.0',
+              transform:'translateX(0px)',
+              color:'#f00'}
+          }}>
+          <div style={{color:'inherit'}}>Hello World!</div>
+        </SparkScroll>
+      </div>
+    );
+  }
+});
+
+React.render(<App/>, document.getElementById('example'));
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+var webpack = require('webpack');
+
+var plugins = [
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+  })
+];
+
+if (process.env.COMPRESS) {
+  plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  );
+}
+
+module.exports = {
+
+  output: {
+    library: 'ReactSparkScroll',
+    libraryTarget: 'umd'
+  },
+
+  resolve: {
+    alias: {
+      underscore: 'lodash'
+    }
+  },
+
+  externals: [
+    {
+      "react": {
+        root: "React",
+        commonjs2: "react",
+        commonjs: "react",
+        amd: "react"
+      }
+    }
+  ],
+
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader' }
+    ]
+  },
+
+  node: {
+    Buffer: false
+  },
+
+  plugins: plugins
+
+};
