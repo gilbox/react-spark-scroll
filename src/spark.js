8a30c81dc3320d5e30355ad074f82666d1472a95
gilbox <birmangil@gmail.com>
2015-03-29 18:40:01 -0500

    todo re: sparkAnimator instantiation

--- src/spark.js
+++ src/spark.js
@@ -17,7 +17,9 @@ function spark(element, timeline) {
   var minScrollY = 0;
   var maxScrollY = 0;
 
+  // @todo: move this out of here (it should only happen 1x)
   var animator = hasAnimateAttr && sparkAnimator.instance();
+  
   var actor = isAnimated && animator.addActor({
       context: element
     });
