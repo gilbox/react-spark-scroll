814618d8afe31438656ae826234ab91b3b3f85e1
gilbox <birmangil@gmail.com>
2015-08-20 00:30:26 -0700

    demo-functional: tween.js reorder stuff and comment

--- examples/demo-functional/lib/tween.js
+++ examples/demo-functional/lib/tween.js
@@ -1,3 +1,17 @@
+/*
+* The following concepts are essential to this code:
+*
+* A VALUE FACTORY is a function which 
+*   - accepts any number of arguments representing the desired value and 
+*   - returns a WRAPPED VALUE object 
+*
+* A WRAPPED VALUE is an object with a `tween` method and a `resolveValue` method.
+*   - `resolveValue` returns the formatted string representation of the value.
+*   - `tween` returns the formatted string representation of the result of tweening
+*     two different WRAPPED VALUEs created with the same VALUE FACTORY.
+*
+*/
+
 export const isNumber = x => typeof x === 'number';
 export const isWrapped = x => !!x.tween;
 export const isNotWrapped = x => !x.tween;
@@ -29,24 +43,25 @@ export const resolveValue = x =>
   isNumber(x) ? x :
     x::mapObject(resolveValue); // is object
 
-export function combine(...wrappedValues) {
-  return {
-    wrappedValues,
-    resolveValue() {
-      return wrappedValues.map(resolveValue).join(' ');
-    },
-    tween(progress, 
-      {wrappedValues: wrappedValuesA}, 
-      {wrappedValues: wrappedValuesB}
-    ) {
-      return wrappedValuesA
-        .map((wrappedValueA, index) => 
-          tweenValues(progress, wrappedValueA, wrappedValuesB[index]))
-        .join(' ');
-    }
-  }
-}
-    
+/**
+ * tween
+ * `position` is a number representing the current timeline position
+ *
+ * `keyframes` is an object where 
+ *   - the properties are numbers
+ *     representing keyframe positions on the timeline. Note that your
+ *     keyframes must *already* be sorted, `tween` will **not** sort them for you.
+ *   - the values are either numbers, objects, or wrapped values (wrapped values may also be nested)
+ *       * when the values are numbers `tween` returns a (tweened) Number
+ *       * when the values are objects `tween` returns an object.
+ *       * when the values are wrapped values `tween` returns the resolved result of the wrapped 
+ *         value (usually a string) 
+ *  Note that all Keyframe values should be exactly the same type or shape.
+ *
+ * `ease` is an (optional) easing function which should accept a number 0..1
+ * and return a number usually 0..1 but for certain types of easing
+ * you might want to go outside of the 0..1 range
+ */
 export function tween(position, keyframes, ease=identity) {
   const positions = Object.keys(keyframes);
   const position0 = positions[0];
@@ -76,6 +91,18 @@ export function tween(position, keyframes, ease=identity) {
     keyframes[positionB])
 }
 
+/** 
+ * createTweenValueFactory 
+ * The first argument, `formatter` should be a 1-arity function 
+ * which accepts an array (`value`) and returns the formatted result.
+ * For example, `formatter` might transform the array `[100,0,255]` to "rgb(100,0,255)"
+ *
+ * The second (optional) argument, `defaultWrapper` will 
+ * be used to map the elements of the `value` array which is useful
+ * for wrapping the values in a default unit (like px, %, deg, etc)
+ *
+ * return a value factory.
+ */
 export function createTweenValueFactory(formatter, defaultWrapper) {
   return (...value) => {
     if (defaultWrapper) 
@@ -89,6 +116,38 @@ export function createTweenValueFactory(formatter, defaultWrapper) {
       resolveValue() {
         return formatter(value.map(resolveValue))
       }
+    }
+  }
+}
+
+/**
+ * combine is a value factory that combines wrapped values (or numbers)
+ * by seperating them with a space
+ *
+ * for example:
+ *
+ *        combine(scale(0.9), translate3d(0,-160,0))
+ *
+ * note that `scale(0.9)` and `translate3d(0,-160,0)`
+ * both return wrapped values. So in the non-tweened case,
+ * combine produces:
+ * 
+ *        "scale(0.9) translate3d(0,-160,0)"
+ */
+export function combine(...wrappedValues) {
+  return {
+    wrappedValues,
+    resolveValue() {
+      return wrappedValues.map(resolveValue).join(' ');
+    },
+    tween(progress, 
+      {wrappedValues: wrappedValuesA}, 
+      {wrappedValues: wrappedValuesB}
+    ) {
+      return wrappedValuesA
+        .map((wrappedValueA, index) => 
+          tweenValues(progress, wrappedValueA, wrappedValuesB[index]))
+        .join(' ');
     }
   }
 }
